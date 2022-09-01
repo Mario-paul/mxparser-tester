9cbeee4c36fa123325ff66e524c9d60c9940cba1
Mario-paul <15961362+Mario-paul@users.noreply.github.com>
2022-09-01 14:57:59 -0400

    add enable viewBinding

--- app/src/main/java/com/example/mxparsertester/MainActivity.kt
+++ app/src/main/java/com/example/mxparsertester/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.mxparsertester
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mxparsertester.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
